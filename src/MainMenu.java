7f3b3601c7c18bd781c0f2c3154d5a2a6055f3aa
MajidTal <MajidAlhadidi@outlook.com>
2023-01-23 11:43:55 +0400

    display menu and let user insert number

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
 
 public class MainMenu {
 
 	public static void main(String[] args) {
 
 		
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Welcome To Out System");
+		System.out.println("---------Welcome To Our System---------");
+		System.out.println("\npress 1: to Join in Shop Settings");
+		System.out.println("press 2: to Manage Shop Items");
+		System.out.println("press 3: to Create New Invoice");
+		System.out.println("press 4: to Report:View Statistics of Report ");
+		System.out.println("press 5: to Report:To view All Invoices");
+		System.out.println("press 6: to Search (1)Invoice");
+		System.out.println("press 7: to Program Statistics");
+		System.out.println("press 8: to Exit");
 		
-		
+		System.out.println("Select the number please");
+		int a = sc.nextInt();
+
 	}
 
 }
